d81f94c06ff78ac61530caa34331bd69ba71b2e7
Devayani <Devyanighike@yahoo.co.in>
2019-10-04 13:26:21 +0530

    this is commit from asian time zone

---
+++ GithubPractice/src/demo/gitDemo.java
@@ -0,0 +1,12 @@
+package demo;
+public class gitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Hello world");
+		System.out.print("push code	");
+
+	}
+
+}
