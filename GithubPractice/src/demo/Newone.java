66de8a97f23aeab16a9364bdb20396fb0dc9601e
Devayani <Devyanighike@yahoo.co.in>
2019-10-04 15:21:26 +0530

    architech asian development

---
+++ GithubPractice/src/demo/Newone.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class Newone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- GithubPractice/src/demo/gitDemo.java
+++ GithubPractice/src/demo/gitDemo.java
@@ -6,6 +6,9 @@ public class gitDemo {
 
 		System.out.print("Hello world");
 		System.out.print("push code	");
+		System.out.print("push code2_devayani	");
+		System.out.print("push code	3_automation");
+
 
 	}
 
